bfc91544282bd3db3bfedf8f519def28ed279bd9
IvsonSantana <Ivson2323@gmail.com>
2024-09-05 21:06:14 -0300

    modified:   index.html 	modified:   manifest.json 	modified:   service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,44 @@
-self.addEventListener('install', (e) => {
-    e.waitUntil(
-        caches.open('digimon-store').then((cache) => cache.addAll([
-            '/index.html',
-            '/styles.css',
-            '/app.js',
-            '/manifest.json',
-            '/icons/playstore.png'
-            
-        ]))
-    );
+const CACHE_NAME = 'digimonstore';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  '/manifest.json'
+];
+
+// Instalando o Service Worker e armazenando em cache
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-    e.respondWith(
-        caches.match(e.request).then((response) => response || fetch(e.request))
-    );
+// Interceptando requisições para servir conteúdo do cache
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// Atualizando o cache quando o Service Worker é ativado
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
